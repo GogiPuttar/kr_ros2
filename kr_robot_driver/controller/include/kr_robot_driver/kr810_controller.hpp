65840eecfa8fbf8644b8c7171f0aa973caaf0972
Aditya Nair <aditya.nair0123@gmail.com>
2025-07-24 18:40:33 +0000

    Patched code with comments to build with ros2 jazzy

--- kr_robot_driver/controller/include/kr_robot_driver/kr810_controller.hpp
+++ kr_robot_driver/controller/include/kr_robot_driver/kr810_controller.hpp
@@ -15,6 +15,10 @@
 #ifndef KR_ROBOT_DRIVER_KR810_CONTROLLER_HPP_
 #define KR_ROBOT_DRIVER_KR810_CONTROLLER_HPP_
 
+#ifndef CONTROLLER_INTERFACE_PUBLIC
+  #define CONTROLLER_INTERFACE_PUBLIC
+#endif
+
 #include <chrono>
 #include <memory>
 #include <mutex>
@@ -42,43 +46,43 @@ namespace kr_robot_driver
 class RobotController : public controller_interface::ControllerInterface
 {
 public:
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   RobotController();
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::return_type update(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::CallbackReturn on_init() override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::CallbackReturn on_configure(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::CallbackReturn on_deactivate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::CallbackReturn on_cleanup(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::CallbackReturn on_error(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  CONTROLLER_INTERFACE_PUBLIC
+  // CONTROLLER_INTERFACE_PUBLIC
   controller_interface::CallbackReturn on_shutdown(
     const rclcpp_lifecycle::State & previous_state) override;
 
